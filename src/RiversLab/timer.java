88a28e77ed3e4ec6e614cb1cd4dcc45184294e01
RiverRobins <rivergrahamrobins@gmail.com>
2019-12-17 08:35:02 -0600

    saving work, making calander

--- src/RiversLab/timer.java
+++ src/RiversLab/timer.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Scanner;
+import java.util.*;
 import java.time.LocalTime;
 import java.time.LocalDate;
 
@@ -28,12 +28,20 @@ public class timer {
 
     public static void checkForEvents() {
         if (!Files.exists(events)) {
+            System.out.println("File does not exist!");
             try {
                 Files.createFile(events);
+                System.out.println("Making file!");
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static String formatEvent(String n){
+        String first = n.substring(0,n.indexOf("|"));
+        String last = n.substring(n.indexOf("|") + 1);
+        return "Date: " + first + "  Event: " + last;
     }
 
     public static void main(String[] args) {
@@ -94,6 +102,12 @@ public class timer {
                     boolean calGo = true;
 
                     checkForEvents();
+                    List<String> allEvents = new ArrayList<>();
+                    try {
+                        allEvents = Files.readAllLines(events);
+                    } catch (Exception e){
+                        System.out.println("error");
+                    }
 
                     while (calGo){
                         System.out.println("Calender:\n" +
@@ -113,11 +127,55 @@ public class timer {
                                 break;
                             }
                             case "2":{
-                                System.out.println("2");
+                                try {
+                                    for (String it : allEvents) {
+                                        System.out.println(formatEvent(it));
+                                    }
+                                } catch (Exception e){
+                                    System.out.println("Error");
+                                }
                                 break;
                             }
                             case "3":{
-                                System.out.println("3");
+//                                System.out.println("3");
+                                System.out.println("\nEdit Events:\n" +
+                                        "0: Cancel/back\n" +
+                                        "00: Delete an item\n" +
+                                        "000: Add a new event\n" +
+                                        "\n" +
+                                        "Select an existing event to change details:\n");
+                                for (int i = 0; i < allEvents.size(); i++) {
+                                    System.out.println(" "+(i + 1)+": "+formatEvent(allEvents.get(i)));
+                                }
+                                Scanner scan = new Scanner(System.in);
+                                String input = scan.nextLine();
+                                switch (input){
+                                    case "0":{
+                                        break;
+                                    }
+                                    case "00":{
+                                        System.out.println("Enter an item number to delete");
+                                        while (true) {
+                                            try {
+                                                input = scan.nextLine();
+                                                allEvents.remove(Integer.parseInt(input));
+                                                break;
+                                            } catch (Exception e){
+                                                System.out.println("Invalid input");
+                                            }
+                                        }
+                                        break;
+                                    }
+                                    default:{
+                                        if (Integer.parseInt(input) < allEvents.size() & Integer.parseInt(input) > 0){
+                                            System.out.println("editing " + formatEvent(allEvents.get(Integer.parseInt(input) - 1)));
+                                            allEvents.remove(Integer.parseInt(input) - 1);
+                                        } else {
+                                            System.out.println("Invalid input");
+                                        }
+                                        break;
+                                    }
+                                }
                                 break;
                             }
                             default:{
